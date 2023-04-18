681419066dea61491b9309a8d45ad2c5c7fac50f
leewebstechnologies <aliuajibade317@gmail.com>
2023-04-18 17:35:31 +0100

    sidebar added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,9 +3,29 @@ import "./topbar.css";
 const Topbar = () => {
   return (
     <div className="topbar">
-      <div className="topbarLeft">l</div>
-      <div className="topbarCenter">c</div>
-      <div className="topbarRight">r</div>
+      <div className="topbarLeft">
+        <i className="topbarIcon fa-brands fa-square-facebook"></i>
+        <i className="topbarIcon fa-brands fa-square-twitter"></i>
+        <i className="topbarIcon fa-brands fa-square-pinterest"></i>
+        <i className="topbarIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topbarCenter">
+        <ul className="topbarList">
+          <li className="topbarListItem">HOME</li>
+          <li className="topbarListItem">ABOUT</li>
+          <li className="topbarListItem">CONTACT</li>
+          <li className="topbarListItem">WRITE</li>
+          <li className="topbarListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topbarRight">
+        <img
+          className="topbarImg"
+          src="https://cdn.britannica.com/99/236599-050-1199AD2C/Mark-Zuckerberg-2019.jpg"
+          alt=""
+        />
+        <i className="topbarSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 };
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      Home
+    </div>
+  );
+};
+
+export default Home;
