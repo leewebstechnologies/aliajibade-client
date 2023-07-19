04fda0e920e41cf813789fcbd819d1a62e8c4f38
leewebstechnologies <aliuajibade317@gmail.com>
2023-07-19 21:46:31 +0100

    front end

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -13,26 +13,22 @@ const Post = () => {
           <span className="postCategory">Arabic</span>
           <span className="postCategory">Islamic Monotheism</span>
         </div>
-        <span className="postTitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </span>
+        <span className="postTitle">The concept of black magic in Islam</span>
         <hr />
         <span className="postDtae">1 hour ago</span>
       </div>
       <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-        perspiciatis labore ipsam, harum quia ea ipsum facilis alias distinctio
-        quibusdam mollitia perferendis vel cumque quis dolore sint aliquid
-        aspernatur. Iste. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Commodi perspiciatis labore ipsam, harum quia ea ipsum facilis
-        alias distinctio quibusdam mollitia perferendis vel cumque quis dolore
-        sint aliquid aspernatur. Iste. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Commodi perspiciatis labore ipsam, harum quia ea ipsum
-        facilis alias distinctio quibusdam mollitia perferendis vel cumque quis
-        dolore sint aliquid aspernatur. Iste. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Commodi perspiciatis labore ipsam, harum
-        quia ea ipsum facilis alias distinctio quibusdam mollitia perferendis
-        vel cumque quis dolore sint aliquid aspernatur. Iste.
+        In the terminology of the Holy Quran and Hadeeth, Sihr (magic) refers to
+        all those usual happenings which have been brought about with the active
+        help of the devils (Shayateen), won over through certain practices
+        likely to please them. There are several ways of pleasing the devils.
+        Sometimes one sings in the praises of the devils, and sometimes recites
+        formulas of a heretical nature which deny Allah or associate others with
+        him. The worship of stars and planets also pleases the devils, as do
+        certain evil deeds for example, preparing a charm with the blood of a
+        person killed unlawfully, or refusing to adopt the prescribed modes of
+        purifying oneself, or living constantly in the state of impurity.
+        (Ma’ariful Quran p 270 v 1)
       </p>
     </div>
   );
